683445c9215cbc8f220822a7ec01d8bb2924f337
Michael Conrad <michael@threepears.com>
2016-02-18 10:01:47 -0600

    Remove ESLint

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
