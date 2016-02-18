b40d97427a5598aec85274deb0e0a6b501ee7fd3
Michael Conrad <michael@threepears.com>
2016-02-18 09:53:10 -0600

    Create new project with express server

---
+++ server.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.send('Server Running');
+});
+
+app.listen(port, () => {
+	console.log(`Evernode server running on port: ${port}`);
+})
