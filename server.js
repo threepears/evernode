b119d4e370ebe1b593a678ec64156745a76c32f4
Michael Conrad <michael@threepears.com>
2016-02-18 10:54:10 -0600

    Implement note creation, save to database

--- server.js
+++ server.js
@@ -5,12 +5,40 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
+const Note = mongoose.model('Notes', mongoose.Schema({
+	title: String,
+	text: String
+}));
+
+// All middleware comes before routes
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 
 app.get('/', (req, res) => {
 	res.send('Server Running');
 });
 
-app.listen(port, () => {
-	console.log(`Evernode server running on port: ${port}`);
-})
+app.get('/notes/new', (req, res) => {
+	res.render('new-note');
+});
+
+app.post('/notes', (req, res) => {
+	Note.create(req.body, (err, note) => {
+		if (err) throw err;
+		console.log(note);
+		res.redirect('/');
+	});
+});
+
+mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
+	if (err) throw err;
+
+	app.listen(PORT, () => {
+  	console.log(`Evernode server running on port ${PORT}`);
+  });
+});
